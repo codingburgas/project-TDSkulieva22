b3b1d62d3209fcf62243e417aeebd10cf4dd2bfd
TDSkulieva <TDSkulieva22@codingburgas.bg>
2026-04-10 16:31:25 +0300

    Fix errors with login and register buttons and design.

--- Recipe_Hub/Recipe_Hub/Program.cs
+++ Recipe_Hub/Recipe_Hub/Program.cs
@@ -1,29 +1,37 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Recipe_Hub.Data;
 using Recipe_Hub.Services; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
                        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = true;
-        options.Password.RequireDigit = false;
-    }
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = true;
+            options.Password.RequireDigit = false;
+        }
     )
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<IRecipeService, RecipeService>();
 
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -42,6 +50,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
@@ -49,4 +59,39 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+    string adminEmail = "admin@a";
+    string adminPassword = "Admin123@";
+
+    var user = await userManager.FindByEmailAsync(adminEmail);
+    
+    if (user == null)
+    {
+        user = new IdentityUser
+        {
+            UserName = adminEmail,
+            Email = adminEmail,
+            EmailConfirmed = true
+        };
+        await userManager.CreateAsync(user, adminPassword);
+    }
+
+    if (await roleManager.FindByNameAsync("Admin") == null)
+    {
+        await roleManager.CreateAsync(new IdentityRole("Admin"));
+    }
+    
+    if (!await userManager.IsInRoleAsync(user, "Admin"))
+    {
+        await userManager.AddToRoleAsync(user, "Admin");
+    }
+}
+
 app.Run();
+
---
+++ Recipe_Hub/Recipe_Hub/Services/EmailSender.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+
+namespace Recipe_Hub.Services;
+
+public class EmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        return Task.CompletedTask;
+    }
+}
